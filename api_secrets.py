5e75eaa92b71c12268d09c126961fc85bed1fa65
acDante <shana0123@163.com>
2024-01-11 23:46:16 +0000

    Experiments on the new test data

--- api_secrets.py
+++ api_secrets.py
@@ -1,2 +1,2 @@
 def get_api_key():
-    return "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"
+    return ""
--- scripts/eval.sh
+++ scripts/eval.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+# Activate conda env
+source ~/.bashrc
+conda activate ~/env/mlkc
+
+echo "Activated conda environment"
+
 SCHEMA=("base" "attr" "instr" "opin" "instr+opin")
 MODEL=("chat")  # TODO: use "base" to run experiments with Llama2-7b
 DEMO=("original" "counter")  # Demo mode
@@ -9,12 +15,12 @@ for p1 in "${DEMO[@]}"; do
     for p2 in "${SCHEMA[@]}"; do
         set -x;
         python3 knowledge_conflict.py --model_name meta-llama/Llama-2-7b-chat-hf \
-                                    --orig_path ./datasets/nq_llama2/orig_dev_filtered.json \
-                                    --counter_path ./datasets/nq_llama2/conflict_dev_filtered.json \
+                                    --orig_path ./datasets/llama_nq_data/nq_llama_orig.json \
+                                    --counter_path ./datasets/llama_nq_data/nq_llama_conflicted.json \
                                     --schema ${p2} \
                                     --demo_mode ${p1} \
                                     --num_demos 16 \
-                                    --log_path results/filtered_nq_llama2 \
+                                    --log_path results/llama2_nq_data \
                                     --exp_name llama2-7b-chat-demo-${p1}-prompt-${p2}
     done
 done
