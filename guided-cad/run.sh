d1c05c8b364ae509d3c2b1720b7d3d97114cbcf0
acDante <shana0123@163.com>
2024-03-15 01:59:29 +0000

    Script for running attribution-based extractive summarisation

---
+++ guided-cad/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Activate conda env
+source ~/.bashrc
+conda activate inseq
+
+attribution_methods=("attention" "saliency")
+# attribution_methods=("deeplift" "gradient_shap") 
+
+for attribution_method in "${attribution_methods[@]}"
+do
+    python extract_sents.py --model_name google/flan-t5-base --num_samples 2500 --num_sents 3 --save_path results/extra_cnn-flan-t5-base-"$attribution_method"-new.json --attribution $attribution_method
+done
